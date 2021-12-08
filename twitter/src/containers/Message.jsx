ada0e2f5baffa11d9188e14a0a9d52f1ff9d5391
javyer <javyermurillo1@gmail.com>
2021-12-07 21:56:16 -0600

    first commit

---
+++ twitter/src/containers/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menssage = () => {
+    <div>
+        <h1>Menssage</h1>
+    </div>
+}
+
+export default Menssage;
---
+++ twitter/src/routes/App.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { BrowserRouter,Route, Switch } from "react-router-dom";
+// import {Home, Profile, Message } from '../containers';
+
+// const App = () => {
+//   const NotFound = () => <h1>404</h1>;
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/profile" component={Profile} />
+//         <Route exact path="/message" component={Message} />
+//         <Route component={NotFound} />
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
+
+import React from "react";
+import '../App.css';
+const App = () => {
+  <>
+  <div>
+    <div>
+      
+    </div>
+  </div>
+  <Footer/>
+  </>
+}
+
+export default App;
